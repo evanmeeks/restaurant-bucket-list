8f362446c72e49806a695c8119f5e7e0ed6d7537
Evan Meeks <evan.meeks.cdk@gmail.com>
2025-06-13 17:05:13 -0500

    Delete react-versions-fix.sh

--- react-versions-fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# List all React and React Native dependencies
-echo "Checking React dependencies in project..."
-find . -name "node_modules" -type d -prune -o -name "package.json" -type f -exec grep -l "react\|react-native\|expo" {} \;
-
-# Show the actual versions
-echo "Current React versions:"
-npm list react react-dom react-native expo
